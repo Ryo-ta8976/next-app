7dd3a65b356f6c085dfacf522dcc0617bbf64f61
Ryota <0vr6551b2mb422g@gmail.com>
2022-05-06 10:19:50 +0900

    グラフコンポーネント追加

---
+++ src/atoms/graph.tsx
@@ -0,0 +1,56 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import faker from 'faker'
+import type { NextPage } from 'next'
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+import styles from '@/styles/Home.module.css'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: '都道府県別人口数の推移',
+    },
+  },
+}
+
+const labels = ['1980年', '1990年', '2000年', '2010年', '2020年']
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+}
+
+const Graph: NextPage = () => {
+  return <Line options={options} data={data} />
+}
+
+export default Graph
